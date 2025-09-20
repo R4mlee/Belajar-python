7675d6c641d94c144bde297971bf42af1caa9125
R4mlee <ramadhanharry009@gmail.com>
2025-09-20 23:59:59 +0700

    menambahkan fungsi menu & exit program di libs

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
 import random
-from libs import welcome_message
+from libs import welcome_message, exit_program
 
 welcome_message()
+
+def menu():
+    print("Program tersedia saat ini: \n1.Game Tebak Angka \n2.Keluar Program \n\n")
+    chose = int(input("Pilih program yang akan kamu jalankan: "))
+
+    if chose == 1:
+        pass
+    else:
+        exit_program()
+        
+def main():
+    welcome_message()
+    menu()
+    
+if __name__ == "__main__":
+    main()
